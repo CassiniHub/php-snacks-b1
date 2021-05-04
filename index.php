a09c35c729118a56c8bd32fe6c7e7a95f946827f
Marco Orazio Re <marcooraziore@gmail.com>
2021-05-04 16:19:16 +0200

    After review
    - Modified if condition to be more precise and avoid limit cases errors

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
             $mail              = $_GET['mail'];
             $age               = $_GET['age'];
             
-            if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)){
+            if (strlen($name) > 3 && strpos($mail, '@') !== false && strpos($mail, '.') !== false && is_numeric($age)){
                 echo 'Accesso riuscito';
             } else {
                 echo 'Accesso negato';
