22adc9ac5f30377921abf9901e4e05dc352951dc
Marco Orazio Re <marcooraziore@gmail.com>
2021-05-04 15:53:02 +0200

    snack #2 completed

--- index.php
+++ index.php
@@ -14,25 +14,42 @@
         // casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
         // Olimpia Milano - Cantù | 55-60
 
-        $db = [
-            [
-                'home' => 'Dallas Mavericks',
-                'away' => 'L.A. Lakers',
-                'homePoints' => 110,
-                'awayPoints' => 107
-            ],
-            [
-                'home' => 'L.A. Clippers',
-                'away' => 'Phoenix Suns',
-                'homePoints' => 103,
-                'awayPoints' => 115
-            ]
-        ];
+        // $db = [
+        //     [
+        //         'home' => 'Dallas Mavericks',
+        //         'away' => 'L.A. Lakers',
+        //         'homePoints' => 110,
+        //         'awayPoints' => 107
+        //     ],
+        //     [
+        //         'home' => 'L.A. Clippers',
+        //         'away' => 'Phoenix Suns',
+        //         'homePoints' => 103,
+        //         'awayPoints' => 115
+        //     ]
+        // ];
 
-        foreach ($db as $match) {
-            echo $match['home'] . ' - ' . $match['away'] . ' | ' . $match['homePoints'] . '-' . $match['awayPoints'];
-            echo '<br>';
-        };
-    ?>
+        // foreach ($db as $match) {
+        //     echo $match['home'] . ' - ' . $match['away'] . ' | ' . $match['homePoints'] . '-' . $match['awayPoints'];
+        //     echo '<br>';
+        // };
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        ## Snack 2
+        // Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
+        // che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
+        // Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"
+
+            $name              = $_GET['name'];
+            $mail              = $_GET['mail'];
+            $age               = $_GET['age'];
+            
+            if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)){
+                echo 'Accesso riuscito';
+            } else {
+                echo 'Accesso negato';
+            };
+        ?>
 </body>
 </html>
